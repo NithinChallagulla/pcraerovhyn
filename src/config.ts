96af88abde49ff866e7c49963e43c1cd5a86a27a
Nithin Challagulla <115912319+NithinChallagulla@users.noreply.github.com>
2026-01-04 17:08:35 +0530

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,15 +1,12 @@
 // src/config.ts
 
+export const STREAM_SERVER = "http://35.193.55.170";
 
-// ================= STREAMING SERVER =================
-// RTMP + HLS + Replay VM
-export const API_BASE = "/api";
-
-// ================= HLS (via Netlify proxy) =================
-export const HLS_BASE = "/hls";
+// ================= API SERVER =================
+export const API_BASE = "http://35.193.55.170:4000";
 
 // ================= ANALYTICS =================
-export const ANALYTICS_BASE = "/analytics-api";
+export const ANALYTICS_BASE = "http://34.93.170.150:8001";
 
 // ================= TYPES =================
 export type StreamStatus = "PENDING" | "LIVE" | "ENDED";
@@ -26,6 +23,7 @@ export interface Stream {
   startedAt?: number | null;
   endedAt?: number | null;
 }
+
 
 export interface AnalyticsResponse {
   streamKey: string;
