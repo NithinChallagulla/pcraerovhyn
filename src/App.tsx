d5a4645c8d3ee189591e2224e361b3052edca217
Nithin Challagulla <115912319+NithinChallagulla@users.noreply.github.com>
2026-01-05 21:24:01 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
+// src/App.tsx
 import { Routes, Route, Navigate } from "react-router-dom";
-
 import Layout from "./components/Layout";
-import ProtectedRoute from "./components/ProtectedRoute";
 
-import Login from "./pages/Login";
 import IncidentBridge from "./pages/IncidentBridge";
 import Master from "./pages/Master";
 import DroneFeeds from "./pages/DroneFeeds";
@@ -12,29 +10,21 @@ import FullscreenFeeds from "./pages/FullscreenFeeds";
 
 export default function App() {
   return (
-    <Routes>
-      {/* Public */}
-      <Route path="/login" element={<Login />} />
+    <Layout>
+      <Routes>
+        {/* Default → /master */}
+        <Route path="/" element={<Navigate to="/master" replace />} />
 
-      {/* Protected */}
-      <Route
-        path="/*"
-        element={
-          <ProtectedRoute>
-            <Layout />
-          </ProtectedRoute>
-        }
-      >
-        <Route index element={<Navigate to="/master" replace />} />
-        <Route path="incident-bridge" element={<IncidentBridge />} />
-        <Route path="master" element={<Master />} />
-        <Route path="feeds" element={<DroneFeeds />} />
-        <Route path="analytics" element={<Analytics />} />
-        <Route path="fullscreen" element={<FullscreenFeeds />} />
-      </Route>
+        {/* Pages */}
+        <Route path="/incident-bridge" element={<IncidentBridge />} />
+        <Route path="/master" element={<Master />} />
+        <Route path="/feeds" element={<DroneFeeds />} />
+        <Route path="/analytics" element={<Analytics />} />
+        <Route path="/fullscreen" element={<FullscreenFeeds />} />
 
-      {/* Fallback */}
-      <Route path="*" element={<Navigate to="/login" replace />} />
-    </Routes>
+        {/* Fallback */}
+        <Route path="*" element={<Navigate to="/master" replace />} />
+      </Routes>
+    </Layout>
   );
 }
