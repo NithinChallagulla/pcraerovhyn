bc0a2078ea3fb028fa2dc8355f9ac4a75a4fb67a
Nithin Challagulla <115912319+NithinChallagulla@users.noreply.github.com>
2026-01-05 21:15:54 +0530

    Update ProtectedRoute.tsx

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,11 +1,16 @@
 import { Navigate } from "react-router-dom";
+import React from "react";
 
-export default function ProtectedRoute({ children }: { children: JSX.Element }) {
+type Props = {
+  children: React.ReactNode;
+};
+
+export default function ProtectedRoute({ children }: Props) {
   const isAuth = localStorage.getItem("aerovhyn_auth") === "true";
 
   if (!isAuth) {
     return <Navigate to="/login" replace />;
   }
 
-  return children;
+  return <>{children}</>;
 }
