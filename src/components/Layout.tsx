0c9a01d66139dc38ff9f6b7c6a1f1ef345e13a59
Nithin Challagulla <115912319+NithinChallagulla@users.noreply.github.com>
2026-01-05 21:30:47 +0530

    Update Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,16 +1,17 @@
 // src/components/Layout.tsx
 import type { ReactNode } from "react";
+import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
 
 interface Props {
-  children: ReactNode;
+  children?: ReactNode; // ✅ OPTIONAL
 }
 
 export default function Layout({ children }: Props) {
   return (
     <>
       <Navbar />
-      {children}
+      {children ?? <Outlet />}
     </>
   );
 }
